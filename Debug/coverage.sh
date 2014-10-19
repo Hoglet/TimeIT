64e9503077ce2a88308251687a141c908ebe1294
Kent Asplund <hoglet@solit.se>
2014-10-19 12:15:56 +0200

    Coverage to jenkins

--- Debug/coverage.sh
+++ Debug/coverage.sh
@@ -2,6 +2,7 @@
 lcov --capture --directory src --output-file  coverage.info
 lcov --extract coverage.info --output-file timeit.coverage "*/src/Sync/*" "*/src/DB/*"  "*/src/Logic/*" "*/src/Misc/*" 
 genhtml timeit.coverage --no-branch-coverage --output-directory .coverage
-firefox .coverage/index.html&
+python lcov_cobertura.py timeit.coverage
 rm coverage.info
-rm timeit.coverage
+rm timeit.coverage 
+
