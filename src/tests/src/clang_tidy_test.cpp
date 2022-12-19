3b12619ff162c49a65bf1719cda5245b7ae8e15e
Kent Asplund <hoglet@solit.se>
2022-12-19 16:15:36 +0100

    Clang tidy check

---
+++ src/tests/src/clang_tidy_test.cpp
@@ -0,0 +1,22 @@
+
+void badNameFunction()
+{
+
+}
+
+void bad_brace_placement() {
+
+}
+
+int bad_parameters(int x, int y)
+{
+	return x + y;
+}
+
+bool missing_braces()
+{
+	if ( true )
+		return false;
+	else
+		return true;
+}
